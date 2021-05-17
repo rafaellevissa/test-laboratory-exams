1c4a563841d986fb9993254f11d49f69f1b8d911
Rafael Levi <rafaellevissa@gmail.com>
2021-05-17 02:27:41 +0100

    feat: added Laboratory's repository and your methods

---
+++ app/Repositories/LaboratoryRepository.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const Laboratory = use('App/Models/Laboratory')
+
+class LaboratoryRepository {
+
+  static async findAll() {
+    const laboratorys = await Laboratory.all()
+    return laboratorys.toJSON()
+  }
+
+  static async store(data) {
+    const laboratory = await Laboratory.create(data)
+    return laboratory
+  }
+
+  static async findByID(id) {
+    const laboratory = await Laboratory.findBy('id', id)
+    return laboratory
+  }
+
+  static async update({ laboratory, data }) {
+    data.name = data.name === null ? laboratory.name : data.name
+    data.address = data.address === null ? laboratory.address : data.address
+    data.status = data.status === null ? laboratory.status : data.status
+
+    laboratory.merge(data)
+    await laboratory.save()
+
+    return laboratory
+  }
+
+  static async destroy(id) {
+    const laboratory = await this.findByID(id)
+    await laboratory.delete()
+  }
+}
+
+module.exports = LaboratoryRepository
